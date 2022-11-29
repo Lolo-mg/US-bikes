66df0434d9544bcd648d8324f7d6bcc6aad01606
Lolo Almuqbil <119347835+Lolo-mg@users.noreply.github.com>
2022-11-29 21:15:53 +0300

    Add files via upload

--- bikeshare.py
+++ bikeshare.py
@@ -148,7 +148,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays statistics of the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -166,7 +166,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df,city):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics for bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
